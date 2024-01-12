82f3f64064b3baaf67d2a2acb94aff64e5ffe7ab
netking79 <156326697+netking79@users.noreply.github.com>
2024-01-13 07:40:53 +0800

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,33 @@
+function functionName() {  
+
+alert("You are learning how to call JavaScript function in html");
+
+        }
+
+function getData() {
+
+	var datas = [];
+
+	datas[0] = 'apple';
+	datas[1] = 'banana';
+	datas[2] = 'orange';
+	datas[3] = 'grape3';
+	datas[4] = 'grape4';
+	datas[5] = 'grape5';
+	datas[6] = 'grape6';
+	datas[7] = 'grape7';
+	datas[8] = 'grape8';
+	datas[9] = 'grape9';
+	datas[10] = 'grape10';
+	datas[11] = 'grape11';
+	datas[12] = 'grape12';
+	datas[13] = '在JavaScript中，你可以通过多种方式向HTML元素添加内容。以下是两种常见的方法：在JavaScript中，你可以通过多种方式向HTML元素添加内容。以下是两种常见的方法：在JavaScript中，你可以通过多种方式向HTML元素添加内容。以下是两种常见的方法：作为一款高效的万能AI助手，我拥有多项强大的功能，可以满足您各种需求。5.无论您需要什么样的服务，在这里都可以得到满足。';
+	datas[14] = 'grape14';
+	datas[15] = 'grape15';
+	datas[16] = 'grape16';
+
+
+	let hasDays = Math.ceil(( new Date() - new Date(new Date().getFullYear().toString()))/(24*60*60*1000))+1;
+	return datas[hasDays];
+	//alert(datas[hasDays]);
+}
